d5decf2ecfd86633342c264e052189b50953a215
Caleb Hart <caleb_hart11@yahoo.com>
2023-03-17 12:39:53 -0700

    edited header

--- gnarbazaar/components/Layout.js
+++ gnarbazaar/components/Layout.js
@@ -9,7 +9,7 @@ export default function Layout({ title, children }) {
         <title>{title ? title + " - Gnarbazaar" : "Gnarbazaar"}</title>
         <meta
           name="description"
-          content="A place to share your awesome designs!"
+          content="A marketplace to share your awesome designs!"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
@@ -18,7 +18,7 @@ export default function Layout({ title, children }) {
         <header>
             <nav className='flex h-12 items-center px-4 justify-between shadow-md'>
                 <Link href="/" className='text-lg font-bold'>
-                    Gnarbazaar
+                    <img className='h-11 w-22' src='https://i.imgur.com/gDkedEM.png'/>
                 </Link>
                 <div>
                     <Link href="/cart" className='p-2'>Cart</Link>
