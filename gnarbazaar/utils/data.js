ca51a708a57b17d758dad4bdec8056d1285cacee
Caleb Hart <caleb_hart11@yahoo.com>
2023-03-17 11:20:07 -0700

    layout wrapper component created, seed data started

---
+++ gnarbazaar/utils/data.js
@@ -0,0 +1,100 @@
+const data = {
+    products: [
+        {
+            name: 'Galaxy Jersey',
+            slug: 'Galaxy Jersey',
+            category: 'shirt',
+            image: '/images/shirt1.jpg',
+            price: 80,
+            brand: 'Deep Dark & Dangerous',
+            rating: 4.8,
+            numReviews: 12,
+            stockAmount: 20,
+            description: 'The latest soccer jersey in our product line'
+        },
+        {
+            name: 'Galaxy Jersey',
+            slug: 'Galaxy Jersey',
+            category: 'shirt',
+            image: '/images/shirt1.jpg',
+            price: 80,
+            brand: 'Deep Dark & Dangerous',
+            rating: 4.8,
+            numReviews: 12,
+            stockAmount: 20,
+            description: 'The latest soccer jersey in our product line'
+        },
+        {
+            name: 'Galaxy Jersey',
+            slug: 'Galaxy Jersey',
+            category: 'shirt',
+            image: '/images/shirt1.jpg',
+            price: 80,
+            brand: 'Deep Dark & Dangerous',
+            rating: 4.8,
+            numReviews: 12,
+            stockAmount: 20,
+            description: 'The latest soccer jersey in our product line'
+        },
+        {
+            name: 'Galaxy Jersey',
+            slug: 'Galaxy Jersey',
+            category: 'shirt',
+            image: '/images/shirt1.jpg',
+            price: 80,
+            brand: 'Deep Dark & Dangerous',
+            rating: 4.8,
+            numReviews: 12,
+            stockAmount: 20,
+            description: 'The latest soccer jersey in our product line'
+        },
+        {
+            name: 'Galaxy Jersey',
+            slug: 'Galaxy Jersey',
+            category: 'shirt',
+            image: '/images/shirt1.jpg',
+            price: 80,
+            brand: 'Deep Dark & Dangerous',
+            rating: 4.8,
+            numReviews: 12,
+            stockAmount: 20,
+            description: 'The latest soccer jersey in our product line'
+        },
+        {
+            name: 'Galaxy Jersey',
+            slug: 'Galaxy Jersey',
+            category: 'shirt',
+            image: '/images/shirt1.jpg',
+            price: 80,
+            brand: 'Deep Dark & Dangerous',
+            rating: 4.8,
+            numReviews: 12,
+            stockAmount: 20,
+            description: 'The latest soccer jersey in our product line'
+        },
+        {
+            name: 'Galaxy Jersey',
+            slug: 'Galaxy Jersey',
+            category: 'shirt',
+            image: '/images/shirt1.jpg',
+            price: 80,
+            brand: 'Deep Dark & Dangerous',
+            rating: 4.8,
+            numReviews: 12,
+            stockAmount: 20,
+            description: 'The latest soccer jersey in our product line'
+        },
+        {
+            name: 'Galaxy Jersey',
+            slug: 'Galaxy Jersey',
+            category: 'shirt',
+            image: '/images/shirt1.jpg',
+            price: 80,
+            brand: 'Deep Dark & Dangerous',
+            rating: 4.8,
+            numReviews: 12,
+            stockAmount: 20,
+            description: 'The latest soccer jersey in our product line'
+        },
+    ]
+}
