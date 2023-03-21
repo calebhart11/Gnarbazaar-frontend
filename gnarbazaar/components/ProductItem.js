6af61246073cedaf4cfc08ce234668982777215c
Caleb Hart <caleb_hart11@yahoo.com>
2023-03-20 19:25:15 -0700

    product component created, show page works

---
+++ gnarbazaar/components/ProductItem.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import React from "react";
+import products from "@/utils/data";
+
+export default function ProductItem({ product }) {
+  return (
+    <div className="card">
+      <Link href={`/product/${product.slug}`}>
+        <img
+          src={product.image}
+          alt={product.name}
+          className="rounded shadow"
+        />
+      </Link>
+      <div className="flex flex-column items-center justify-center">
+        <Link href={`/product/${product.slug}`}>
+          <h2 className="text-lrg">{product.name}</h2>
+
+          <p className="mb-2">{product.brand}</p>
+          <p>${product.price}</p>
+          <button className="primary-button" type="button">
+            Add to cart
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
--- gnarbazaar/utils/data.js
+++ gnarbazaar/utils/data.js
@@ -4,13 +4,61 @@ const data = {
             name: 'Galaxy Jersey',
             slug: 'Galaxy Jersey',
             category: 'shirt',
-            image: '/images/shirt1.jpg',
+            image: 'https://i.imgur.com/ScuQ2JH.jpg',
             price: 80,
             brand: 'Deep Dark & Dangerous',
             rating: 4.8,
             numReviews: 12,
             stockAmount: 20,
             description: 'The latest soccer jersey in our product line'
+        },
+        {
+            name: 'Fidget T-Shirt',
+            slug: 'Fidget T-Shirt',
+            category: 'shirt',
+            image: 'https://i.imgur.com/Np6apQc.jpg',
+            price: 50,
+            brand: 'Ivy Lab',
+            rating: 4.5,
+            numReviews: 10,
+            stockAmount: 8,
+            description: 'Official merch for the Fidget E.P'
+        },
+        {
+            name: 'Sigil Joggers',
+            slug: 'Sigil Joggers',
+            category: 'pants',
+            image: 'https://i.imgur.com/1yBCyQL.jpg',
+            price: 90,
+            brand: 'Shades',
+            rating: 5.0,
+            numReviews: 20,
+            stockAmount: 30,
+            description: 'Hand embroidered, very high quality'
+        },
+        {
+            name: 'Winter line Hoodie',
+            slug: 'Winter line Hoodie',
+            category: 'outerwear',
+            image: 'https://i.imgur.com/yMsndxC.jpg',
+            price: 60,
+            brand: 'CharlesTheFirst',
+            rating: 5.0,
+            numReviews: 22,
+            stockAmount: 4,
+            description: 'Acid wash hoodie from the winter merch drop'
+        },
+        {
+            name: 'Logo Beanie',
+            slug: 'Logo Beanie',
+            category: 'headwear',
+            image: 'https://i.imgur.com/Jr17weW.jpg',
+            price: 35,
+            brand: 'Of The Trees',
+            rating: 4.6,
+            numReviews: 6,
+            stockAmount: 40,
+            description: 'Beanie with the Of The Trees logo'
         },
         {
             name: 'Galaxy Jersey',
@@ -24,77 +72,7 @@ const data = {
             stockAmount: 20,
             description: 'The latest soccer jersey in our product line'
         },
-        {
-            name: 'Galaxy Jersey',
-            slug: 'Galaxy Jersey',
-            category: 'shirt',
-            image: '/images/shirt1.jpg',
-            price: 80,
-            brand: 'Deep Dark & Dangerous',
-            rating: 4.8,
-            numReviews: 12,
-            stockAmount: 20,
-            description: 'The latest soccer jersey in our product line'
-        },
-        {
-            name: 'Galaxy Jersey',
-            slug: 'Galaxy Jersey',
-            category: 'shirt',
-            image: '/images/shirt1.jpg',
-            price: 80,
-            brand: 'Deep Dark & Dangerous',
-            rating: 4.8,
-            numReviews: 12,
-            stockAmount: 20,
-            description: 'The latest soccer jersey in our product line'
-        },
-        {
-            name: 'Galaxy Jersey',
-            slug: 'Galaxy Jersey',
-            category: 'shirt',
-            image: '/images/shirt1.jpg',
-            price: 80,
-            brand: 'Deep Dark & Dangerous',
-            rating: 4.8,
-            numReviews: 12,
-            stockAmount: 20,
-            description: 'The latest soccer jersey in our product line'
-        },
-        {
-            name: 'Galaxy Jersey',
-            slug: 'Galaxy Jersey',
-            category: 'shirt',
-            image: '/images/shirt1.jpg',
-            price: 80,
-            brand: 'Deep Dark & Dangerous',
-            rating: 4.8,
-            numReviews: 12,
-            stockAmount: 20,
-            description: 'The latest soccer jersey in our product line'
-        },
-        {
-            name: 'Galaxy Jersey',
-            slug: 'Galaxy Jersey',
-            category: 'shirt',
-            image: '/images/shirt1.jpg',
-            price: 80,
-            brand: 'Deep Dark & Dangerous',
-            rating: 4.8,
-            numReviews: 12,
-            stockAmount: 20,
-            description: 'The latest soccer jersey in our product line'
-        },
-        {
-            name: 'Galaxy Jersey',
-            slug: 'Galaxy Jersey',
-            category: 'shirt',
-            image: '/images/shirt1.jpg',
-            price: 80,
-            brand: 'Deep Dark & Dangerous',
-            rating: 4.8,
-            numReviews: 12,
-            stockAmount: 20,
-            description: 'The latest soccer jersey in our product line'
-        },
+        
     ]
 }
+export default data
